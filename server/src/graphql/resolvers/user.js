255f4859a88ab285e22cf0c9c922c529e1fab62d
Frosenow <filip.potepa@gmail.com>
2023-08-20 18:52:01 +0200

    added method to register users in DB

--- server/src/graphql/resolvers/user.js
+++ server/src/graphql/resolvers/user.js
@@ -1,11 +1,50 @@
 import User from "../../models/User.mjs";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+config();
 
 const usersResolvers = {
   Mutation: {
-    register(_, args, context, info) {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
       // TODO: Validate user data
       // TODO: Make sure user doesnt already exits
-      // TODO: Hash password and create auth token
+
+      // Hash password and sent to DB
+      password = await bcrypt.hash(password, 12);
+
+      // Create new user with hashed password and creation time
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      // Save user to database
+      const res = await newUser.save();
+
+      // Generate JWT token
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
