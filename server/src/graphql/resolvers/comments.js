ef7de093221619dd75d04f151f00d476f2f9f36f
Frosenow <filip.potepa@gmail.com>
2023-08-26 19:09:00 +0200

    added method to delete comments

--- server/src/graphql/resolvers/comments.js
+++ server/src/graphql/resolvers/comments.js
@@ -1,3 +1,5 @@
+import { AuthenticationError } from "apollo-server";
+
 import Comment from "../../models/Comment.mjs";
 import { authUser } from "../../util/check-auth.js";
 
@@ -26,8 +28,10 @@ const commentsResolvers = {
   },
   Mutation: {
     async createComment(_, { body }, context) {
+      // Authenticate user
       const user = authUser(context);
 
+      // Create comment from passed data
       const newComment = new Comment({
         body,
         user: user.id,
@@ -35,9 +39,27 @@ const commentsResolvers = {
         createdAt: new Date().toISOString(),
       });
 
+      // Save comment to DB
       const comment = await newComment.save();
 
       return comment;
+    },
+    async deleteComment(_, { commentId }, context) {
+      const user = authUser(context);
+
+      try {
+        // Get the commet data from DB
+        const comment = await Comment.findById(commentId);
+        // Check if the user is the owner of the comment
+        if (user.username === comment.username) {
+          await comment.deleteOne();
+          return "Comment deleted successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
--- server/src/util/check-auth.js
+++ server/src/util/check-auth.js
@@ -4,11 +4,18 @@ import { config } from "dotenv";
 config();
 
 const authUser = (context) => {
+  // Get authorization header
   const authHeader = context.req.headers.authorization;
+
+  // Check if the authorization header was passed
   if (authHeader) {
+    // Get only the auth user jwt token
     const token = authHeader.split("Bearer ")[1];
+
+    // Check if the token exists
     if (token) {
       try {
+        // Verify the token by decoding it using jwt key
         const user = jwt.verify(token, String(process.env.JWT_KEY));
         return user;
       } catch (err) {
