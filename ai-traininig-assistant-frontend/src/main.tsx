a1ee3c9f6b7bf69cdfd0252bbb4fa70a54ea4b91
Frosenow <filip.potepa@gmail.com>
2023-10-19 12:27:47 +0200

    setted up apollo provider

--- ai-traininig-assistant-frontend/src/main.tsx
+++ ai-traininig-assistant-frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { createHttpLink } from '@apollo/client/core';
+
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+const httpLink = createHttpLink({
+  // TODO: Change this in production
+  uri: 'http://localhost:5000/',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
