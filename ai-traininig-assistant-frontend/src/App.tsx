792779fdc0006c503f3ffa3859df75ede9985c1a
Frosenow <filip.potepa@gmail.com>
2023-10-21 13:26:22 +0200

    created context provider

--- ai-traininig-assistant-frontend/src/App.tsx
+++ ai-traininig-assistant-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css';
 import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid';
 
 import './App.css';
+import { AuthProvider } from './context/auth';
 import MenuBar from './components/Navigation/MenuBar/MenuBar';
 import Home from './components/views/Home/Home';
 import Login from './components/views/Login/Login';
@@ -11,20 +12,22 @@ import Register from './components/views/Register/Register';
 
 function App() {
   return (
-    <HashRouter>
-      <Grid padded columns={2} stackable stretched>
-        <Grid.Column width={3} style={{ padding: '9rem' }}>
-          <MenuBar />
-        </Grid.Column>
-        <Grid.Column width={12}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-          </Routes>
-        </Grid.Column>
-      </Grid>
-    </HashRouter>
+    <AuthProvider>
+      <HashRouter>
+        <Grid padded columns={2} stackable stretched>
+          <Grid.Column width={3} style={{ padding: '9rem' }}>
+            <MenuBar />
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+            </Routes>
+          </Grid.Column>
+        </Grid>
+      </HashRouter>
+    </AuthProvider>
   );
 }
 
