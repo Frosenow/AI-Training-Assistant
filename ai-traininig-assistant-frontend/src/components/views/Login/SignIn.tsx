e5e390aa88d074f083387cf014077a74658c7fd8
Frosenow <filip.potepa@gmail.com>
2023-10-25 23:53:11 +0200

    changing login view

--- ai-traininig-assistant-frontend/src/components/views/Login/SignIn.tsx
+++ ai-traininig-assistant-frontend/src/components/views/Login/SignIn.tsx
@@ -1,22 +1,26 @@
-import * as React from 'react';
+import { useMutation } from '@apollo/client';
+
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import { Skeleton } from '@mui/material';
+
+import { AuthError } from '../../../types/types';
+import { LOGIN_USER_MUTATION } from './Mutations/loginMutations';
+import { useForm } from '../utils/hooks';
+import { AuthContext } from '../../../context/auth';
 
 function Copyright(props: any) {
-  const context = useContext(AuthContext);
-  const navigate = useNavigate();
-  const [errors, setErrors] = useState<AuthError>({});
-
   return (
     <Typography
       variant="body2"
@@ -35,14 +39,36 @@ function Copyright(props: any) {
 }
 
 export default function SignIn() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  const context = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState<AuthError>({});
+
+  // eslint-disable-next-line @typescript-eslint/no-use-before-define
+  const { onChange, onSubmit, values } = useForm(loginUserCallback, {
+    username: '',
+    password: '',
+  });
+
+  const [loginUser, { loading }] = useMutation(LOGIN_USER_MUTATION, {
+    update(_, { data: { login: userData } }) {
+      setErrors({});
+      context.login(userData);
+      navigate('/', {
+        replace: true,
+      });
+    },
+    async onError(err) {
+      if (err.graphQLErrors[0].extensions.errors) {
+        setErrors(err.graphQLErrors[0].extensions.errors);
+      }
+    },
+    variables: values,
+  });
+
+  // Declared function to call addUser to force hoisting
+  function loginUserCallback() {
+    loginUser();
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -61,27 +87,41 @@ export default function SignIn() {
         <Typography component="h1" variant="h5" color="text.secondary">
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="login"
-            label="Login"
-            name="login"
-            autoComplete="login"
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
+        <Box component="form" onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
+          {loading ? (
+            <Box>
+              <Skeleton />
+              <Skeleton />
+            </Box>
+          ) : (
+            <>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoFocus
+                helperText={errors.username}
+                onChange={onChange}
+                inputProps={{ style: { color: '#000' } }}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                helperText={errors.password}
+                onChange={onChange}
+                inputProps={{ style: { color: '#000' } }}
+              />
+            </>
+          )}
+
           <Button
             type="submit"
             fullWidth
@@ -93,12 +133,21 @@ export default function SignIn() {
           <Grid container>
             <Grid item>
               <Link href="/register#/register" variant="body2">
-                {"Don't have an account? Sign Up"}
+                "Don't have an account? Sign Up"
               </Link>
             </Grid>
           </Grid>
         </Box>
       </Box>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <Copyright sx={{ mt: 8, mb: 4 }} />
     </Container>
   );
